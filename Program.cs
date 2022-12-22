7d1477a4f1e66c510d918ecf99262f473a27d595
Boguckaya_is <Boguckay@mail.ru>
2022-12-22 13:47:15 +0300

    Задача на масив

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array = new string[] { "one", "-2", "hello", "on", "second" }; 
+string[] newarray = new string[array.Length]; 
+void SecondArray(string[] array1, string[] array2) 
+{ 
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i].Length <= 3) 
+        { 
+             newarray[count] = array[i]; 
+             count++; 
+        }  
+    } 
+} 
+
+ 
+ SecondArray(array, newarray); 
+ PrintArray(newarray); 
+ 
+ 
+ void PrintArray(string[] array) 
+ { 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+         Console.Write($"{array[i]} "); 
+    } 
+    Console.WriteLine(); 
+ } 
+
+ SecondArray(array, newarray); 
+ PrintArray(newarray); 
